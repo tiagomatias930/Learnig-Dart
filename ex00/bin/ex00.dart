012b61c245ae13a274f622290f0eeb722d1b1954
Tiago Matias <tiagomatias072@gmail.com>
2026-04-28 04:10:54 -0400

    first task done

---
+++ ex00/bin/ex00.dart
@@ -0,0 +1,5 @@
+import 'package:ex00/ex00.dart' as ex00;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ex00.calculate()}!');
+}
---
+++ ex00/test/ex00_test.dart
@@ -0,0 +1,8 @@
+import 'package:ex00/ex00.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
